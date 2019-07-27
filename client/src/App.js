6b990b979e7d68656a7b51fd653202696f1150dd
Joanne Badua <joannebadua@gmail.com>
2019-07-26 21:46:47 -0700

    Implement BrowserRouter and add Router component with a div of the needed routes rendering Freelancers and Detail components to be displayed one at a time

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
-
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from "react-router-dom";
+import Freelancers from "./pages/Freelancers";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-     
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Freelancers} />
+          <Route exact path="/freelancers" component={Freelancers} />
+          <Route exact path="/freelancers/:id" component={Detail} />
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
