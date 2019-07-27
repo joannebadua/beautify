6da1c1521b4c12d8d846a9b8145669b2ea4afeae
Joanne Badua <joannebadua@gmail.com>
2019-07-26 21:29:41 -0700

    Create freelancers model and require Mongoose

---
+++ models/freelancers.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const freelancerSchema = new Schema({
+  name: { type: String, required: true },
+  service: { type: String, required: true },
+  description: String,
+  price: Integer,
+  date: { type: Date, default: Date.now }
+});
+
+const Freelancer = mongoose.model("Freelancer", freelancerSchema);
+
+module.exports = Freelancer;
