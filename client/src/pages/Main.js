f8c10d230255f495ab67d47b8d6bb0ee94bcf721
Joanne Badua <joannebadua@gmail.com>
2019-08-02 07:49:15 -0700

    Delete freelancers.js and freelancerHome.js bc Main.js is functioning

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -6,46 +6,55 @@ import Col from "../components/Col";
 import SideNav from "../components/Sidenav";
 // import API from "../utils/API";
 
+/*/----------------------------------------
+I'm trying to capture the value in the form ~jb
+----------------------------------------/*/
 //Setting component's initial state
-  state = {
-    service: "",
-    gender: "",
-    time: "",
-    duration: "",
-    day: "",
-    location: ""
-  };
+  // state = {
+  //   service: "",
+  //   gender: "",
+  //   time: "",
+  //   duration: "",
+  //   day: "",
+  //   location: ""
+  // };
 
-// Getting the value and name of the input which triggered the change
-let value = event.target.value;
-var service = event.target.service;
+// // Getting the value and name of the input which triggered the change
+// let value = event.target.value;
+// var service = event.target.service;
 
-if (service === "Make-Up") 
-this.setState({
-  [service]: value
-});
+// if (service === "Make-Up") 
+// this.setState({
+//   [service]: value
+// });
 
-handleFormSubmit = event => {
-//Preventing the default behavior of the form submit  (which is to refresh the page)
-event.preventDefault();
-if (this.state.service) {
-  alert
-}
-};
+// handleFormSubmit = event => {
+// //Preventing the default behavior of the form submit  (which is to refresh the page)
+// event.preventDefault();
+// if (this.state.service) {
+//   alert
+// }
+// }
 
-render() {
-  return (
-    <div>
-      <p>
-        Hello {this.state.firstName} {this.state.lastName}
-      </p>
-        <form className="form">
-          <input 
-  )
-}
+// render() 
+//   return (
+//     <div>
+//       <p>
+//         Hello {this.state.firstName} {this.state.lastName}
+//       </p>
+//         <form className="form">
+//           <input 
+//           value={this.state.service}
+//           name="make-up"
+//           onChange={this.handleInputChange}
+//           type="text"
+//           required
+//           />
+//   )
+
 
 /*/----------------------------------------
-We may have to rename function Main()
+We may have to rename function Main() bc parsing error in return ~jb
 ----------------------------------------/*/
 function Main() {
   return (
