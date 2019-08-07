991c61bfe1e33c65e233756ecf50e32137b3850a
Joanne Badua <joannebadua@gmail.com>
2019-08-07 12:03:46 -0700

    Set up environment var for accessKeyID and secretAccessKey in bash_profile and create profofile.js requiring aws-sdk, multer, multer-s3, path AND function for profile ImgUpload

---
+++ routes/api/profile.js
@@ -0,0 +1,87 @@
+// aws-sdk code
+const aws = require( 'aws=sdk');
+const multerS3 = require( 'multer-s3' );
+const multer = require( 'multer' );
+const path = require( 'path' );
+const url = require( 'url' );
+const router = express.Router();
+const s3 = new aws.S3({
+    accessKeyId: 'AWS_ACCESS_KEY_ID',
+    secretAccessKey: 'AWS_SECRET_ACCESS_KEY',
+    Bucket: 'onclick'
+});
+const profileImgUpload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: 'onclick',
+        acl: 'public-read',
+        key: fuction (req, file, cb) {
+            cb(null,
+            path.basename( file.originalname,
+            path.extname( file.originalname ) ) +
+            '-' + Date.now() +
+            path.extname( file.originalname ) )
+        }
+    }),
+    limits: { fileSize: 20000000 },
+    fileFilter: function( req, file, cb ){
+        checkFileType: (file, cb );
+    }
+}).single('profileImage');
+function checkFileType( file, cb ){
+    const filetypes = /jpeg|jpg|png|gif/;
+    const extname = 
+    filetypes.test( path.extname( file.originalname ).toLowerCase());
+    const mimetype =
+    filetypes.test( file.mimetype );
+    if( mimetype && extname ){
+        return cb( null, true );
+    } else {
+        cb ( 'Error: Images only' );
+    }
+    }
+router.post( '/profile-img-upload', 
+( req, res ) => {
+profileImgUpload ( req, res, ( error ) 
+=> {
+    // console.log( 'request0kokoko', req.file );
+    // console.log( 'error', error );
+    
+    if ( error ){
+    console.log( 'errors', error );
+    res.json( { error: error } );
+            } else {
+                if ( req.file === undefined ){
+                    console.log( 'Error: No File Selected' );
+                    res.json('Error: No File Selected' );
+                } else {
+                    if( req.file === undefined ){
+                        console.log( 'Error: No FIle Selected' );
+                        res.json( 'Error: No FIle Selected' );
+                    } else {
+                        const imageName = req.file.key;
+                        const imageLocation = req.file.location;
+                        res.json( { 
+                            image: imageName,
+                            location: imageLocation
+                        } );
+                    }
+                }
+                } );
+            } );
+        });
+// const uploadsBusinessGallery = multer({
+//     storage: multerS3({
+//         s3: s3,
+//         bucket: 'onlick',
+//         acl: 'public-read',
+//         key: function (req, file, cb) {
+//             cb( null,
+//             path.basename( file.originalname,
+//             path.extname( file.originalname ) ) + 
+//             '-' + Date.now() + 
+//             path.extname( file.originalname ) ) 
+//         ]
+//         })
+// })
+module.exports = router
