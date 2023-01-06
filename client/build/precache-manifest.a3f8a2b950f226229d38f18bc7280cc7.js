0ea1ba353f12768dd023df5fcc8072a8c50ad928
Hallie Calhoun <50391724+halliecalhoun@users.noreply.github.com>
2023-01-06 00:26:39 +0000

    fix build heroku error

---
+++ client/build/precache-manifest.a3f8a2b950f226229d38f18bc7280cc7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d364bbc7091733bdf2f3fd26b5091ac9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "711bde763622f364d98c",
+    "url": "/static/css/2.4ee9d045.chunk.css"
+  },
+  {
+    "revision": "6a2a7fc36f9ae08c4d14",
+    "url": "/static/css/main.117188ca.chunk.css"
+  },
+  {
+    "revision": "711bde763622f364d98c",
+    "url": "/static/js/2.f2f7231e.chunk.js"
+  },
+  {
+    "revision": "6a2a7fc36f9ae08c4d14",
+    "url": "/static/js/main.a0438371.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "815b2dc078ca53f764af5c6c31151da4",
+    "url": "/static/media/blowdry.815b2dc0.png"
+  },
+  {
+    "revision": "6c3c57242e7a7a1686959670c631ddca",
+    "url": "/static/media/fashion.6c3c5724.png"
+  },
+  {
+    "revision": "3d72e2a8e071d6ebbb18287ce42cd2cf",
+    "url": "/static/media/massage.3d72e2a8.png"
+  },
+  {
+    "revision": "7a0aee90d6f6c4e468d73e83b61f8471",
+    "url": "/static/media/yoga2.7a0aee90.png"
+  }
+]);
